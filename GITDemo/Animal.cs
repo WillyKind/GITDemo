4c7e3ce67170daf82715665d8ffed9ad7fddb215
Willy Kind <willykkind@gmail.com>
2016-11-24 10:21:42 +0100

    Adds animal class

---
+++ GITDemo/Animal.cs
@@ -0,0 +1,13 @@
+﻿namespace GITDemo
+{
+    internal class Animal
+    {
+        public Animal(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
